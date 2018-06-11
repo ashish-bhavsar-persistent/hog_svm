11563cd78b80c5b0cfedfb85cff4541d8fcdd14b
Luis M. Zapata <luism.zapatah@gmail.com>
2018-06-11 13:00:31 -0500

    Intial commit

---
+++ blood_cell_detector.py
@@ -0,0 +1,79 @@
+import os
+import cv2
+import xml.etree.ElementTree as ET
+import numpy as np
+
+# Read all XML files
+path = 'BCCD_Dataset'
+xml_files = [(os.path.join(root, name))
+	for root, dirs, files in os.walk(path)
+	for name in files if name.endswith((".xml"))]
+
+# HOG parametrization
+winSize = (32,32)
+blockSize = (16,16)
+blockStride = (4,4)
+cellSize = (8,8)
+nbins = 9
+derivAperture = 1
+winSigma = -1.
+histogramNormType = 0
+L2HysThreshold = 0.2
+gammaCorrection = 1
+nlevels = 64
+useSignedGradients = True
+ 
+hog = cv2.HOGDescriptor(winSize,blockSize,blockStride,
+	cellSize,nbins,derivAperture,winSigma,histogramNormType
+	,L2HysThreshold,gammaCorrection,nlevels, useSignedGradients)
+
+# Retrieve image patches from XML info and images
+features = np.zeros((1,6084),np.float32)
+labels = np.zeros(1,np.int64)
+for t in xml_files:
+	root = ET.parse(t).getroot()
+	img_name = root[1].text
+	img = cv2.imread(path+'/'+img_name)
+	#hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	for elem in root.findall('object'):
+		classes = elem[0].text
+		x1 = int(elem[4][0].text)
+		y1 = int(elem[4][1].text)
+		x2 = int(elem[4][2].text)
+		y2 = int(elem[4][3].text)
+		if x2 > x1 and y2 > y1:
+			if classes == 'RBC':
+				labels = np.vstack((labels, 0))
+			elif classes == 'WBC':
+				labels = np.vstack((labels, 1))
+			elif classes == 'Platelets':
+				labels = np.vstack((labels, 2))
+
+			cropped_img = img[y1:y2, x1:x2]
+			resized_img = cv2.resize(cropped_img, winSize)
+			descriptor = np.transpose(hog.compute(resized_img))
+			features = np.vstack((features, descriptor))
+			print img_name, classes, x1, y1, x2, y2
+			#cv2.imshow(classes, resized_img)
+			#k = cv2.waitKey(0)
+			#if k == ord('q'):
+			#	break
+print features[0,:], labels[0,:]
+features = np.delete(features, (0), axis=0)
+labels = np.delete(labels, (0), axis=0)
+print features[0,:], labels[0,:]
+
+print features.shape, labels.shape
+'''
+img_files = [(os.path.join(root, name))
+	for root, dirs, files in os.walk(path)
+	for name in files if name.endswith((".jpg"))]
+
+images = [cv2.imread(i) for i in img_files]
+
+for i in images:
+	cv2.imshow('ORIGINAL', i)
+	k = cv2.waitKey(0)
+	if k == ord('q'):
+		break
+'''
